05051753b989554b259f90aff6ba7300d1e962ff
Muaz Ahmad <mahmad2000@protonmail.com>
2025-08-20 14:13:21 +0500

    refactor, add dir nav fetching

---
+++ client/client.go
@@ -0,0 +1,187 @@
+package client
+
+import (
+	"crypto/md5"
+
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+type SubsonicClient struct {
+	Server   url.URL
+	Username string
+	Password string
+}
+
+type param struct {
+	query string
+	value string
+}
+
+var rand_src = rand.New(rand.NewSource(time.Now().Unix()))
+
+var ErrBadStatus = fmt.Errorf("Remote server returned non-200 OK response")
+
+func validate_api(r *http.Response) (*subsonicResponse, error) {
+	d := json.NewDecoder(r.Body)
+	var subsonic_r response
+	if err := d.Decode(&subsonic_r); err != nil {
+		return nil, err
+	}
+	if subsonic_r.SubsonicResponse.Error != nil {
+		return nil, fmt.Errorf("%s", subsonic_r.SubsonicResponse.Error.Message)
+	}
+	return &subsonic_r.SubsonicResponse, nil
+}
+
+func (c *SubsonicClient) Ping() error {
+	resp, err := c.get("/ping", nil)
+	if err != nil {
+		return err
+	}
+	if _, err := validate_api(resp); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *SubsonicClient) get(path string, extra_params []param) (*http.Response, error) {
+	req_url := c.Server
+	req_url.Path, _ = url.JoinPath("rest", path)
+	q := make(url.Values)
+	q.Add("f", "json")
+	q.Add("v", "1.16.0")
+	q.Add("c", "subsonic-webui")
+	q.Add("u", c.Username)
+	salt := make([]byte, 8)
+	rand_src.Read(salt)
+	salt_hex := fmt.Sprintf("%x", salt)
+	q.Add("s", salt_hex)
+	token := md5.Sum([]byte(c.Password + salt_hex))
+	q.Add("t", fmt.Sprintf("%x", token))
+	for _, p := range extra_params {
+		q.Add(p.query, p.value)
+	}
+	req_url.RawQuery = q.Encode()
+
+	resp, err := http.Get(req_url.String())
+	if err != nil {
+		return nil, err
+	} else if resp.StatusCode != 200 {
+		return nil, ErrBadStatus
+	}
+	return resp, nil
+}
+
+func (c *SubsonicClient) Indexes() ([]Entry, error) {
+	resp, err := c.get("/getIndexes", nil)
+	if err != nil {
+		return nil, err
+	}
+	if index_resp, err := validate_api(resp); err != nil {
+		return nil, err
+	} else {
+		entries := make([]Entry, 0)
+		for _, i := range index_resp.Indexes.Index {
+			for _, j := range i.Artist {
+				entries = append(entries, Entry{
+					ID:      j.ID,
+					IsDir:   true,
+					Name:    j.Name,
+					Album:   nil,
+					CoverId: nil,
+				})
+			}
+		}
+		for _, i := range index_resp.Indexes.Child {
+			entries = append(entries, Entry{
+				IsDir:   i.IsDir,
+				ID:      i.ID,
+				Name:    i.Title,
+				Album:   i.Album,
+				CoverId: i.CoverId,
+			})
+		}
+
+		return entries, nil
+	}
+}
+
+func (c *SubsonicClient) Dir(id string) ([]Entry, error) {
+	resp, err := c.get("/getMusicDirectory", []param{{
+		query: "id",
+		value: id,
+	}})
+	if err != nil {
+		return nil, err
+	}
+	if dir_resp, err := validate_api(resp); err != nil {
+		return nil, err
+	} else {
+		entries := make([]Entry, 0)
+		for _, i := range dir_resp.Directory.Child {
+			entries = append(entries, Entry{
+				ID:      i.ID,
+				IsDir:   i.IsDir,
+				Name:    i.Title,
+				Album:   i.Album,
+				CoverId: i.CoverId,
+			})
+		}
+
+		return entries, nil
+	}
+}
+
+type response struct {
+	SubsonicResponse subsonicResponse `json:"subsonic-response"`
+}
+
+type subsonicResponse struct {
+	Status    string         `json:"status"`
+	Error     *subsonicError `json:"error"`
+	Indexes   *indexes       `json:"indexes"`
+	Directory *directory     `json:"directory"`
+}
+
+type subsonicError struct {
+	Message string `json:"message"`
+}
+
+type indexes struct {
+	Index []index `json:"index"`
+	Child []child `json:"child"`
+}
+
+type index struct {
+	Artist []artist `json:"artist"`
+}
+
+type artist struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+type child struct {
+	ID      string  `json:"id"`
+	IsDir   bool    `json:"isDir"`
+	Title   string  `json:"title"`
+	Album   *string `json:"album"`
+	CoverId *string `json:"coverArt"`
+}
+
+type Entry struct {
+	ID      string  `json:"id"`
+	IsDir   bool    `json:"isDir"`
+	Name    string  `json:"name"`
+	Album   *string `json:"album,omitempty"`
+	CoverId *string `json:"coverId,omitempty"`
+}
+
+type directory struct {
+	Child []child `json:"child"`
+}
--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"strings"
+	"subsonic-webui/client"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/golang-jwt/jwt/v5"
@@ -16,22 +18,27 @@ import (
 var (
 	//go:embed static/index.html
 	index []byte
+
+	priv_key ed25519.PrivateKey
+	pub_key  ed25519.PublicKey
 )
 
+func init() {
+	pub_key, priv_key, _ = ed25519.GenerateKey(nil)
+}
+
 type CustomJWT struct {
-	auth AuthInfo
+	Auth AuthInfo
 	jwt.RegisteredClaims
 }
 
 type AuthInfo struct {
-	Server   *string `json:"server"`
-	Username *string `json:"username"`
-	Password *string `json:"password"`
+	Server   string `json:"server"`
+	Username string `json:"username"`
+	Password string `json:"password"`
 }
 
 func main() {
-	_, priv_key, _ := ed25519.GenerateKey(nil)
-
 	var listen_addr string
 	if len(os.Args) > 1 {
 		listen_addr = os.Args[1]
@@ -50,21 +57,21 @@ func main() {
 	api_r.Post("/login", func(w http.ResponseWriter, r *http.Request) {
 		d := json.NewDecoder(r.Body)
 		var auth AuthInfo
-		if err := d.Decode(&auth); err != nil || auth.Server == nil || auth.Username == nil || auth.Password == nil {
+		if err := d.Decode(&auth); err != nil {
 			w.WriteHeader(http.StatusBadRequest)
 		} else {
-			server_url, err := url.Parse(*auth.Server)
+			server_url, err := url.Parse(auth.Server)
 			if err != nil {
 				w.WriteHeader(http.StatusBadRequest)
 				return
 			}
 
-			client := SubsonicClient{
-				server:   *server_url,
-				username: *auth.Username,
-				password: *auth.Password,
+			c := client.SubsonicClient{
+				Server:   *server_url,
+				Username: auth.Username,
+				Password: auth.Password,
 			}
-			if err := client.ping(); err != nil {
+			if err := c.Ping(); err != nil {
 				w.WriteHeader(http.StatusBadRequest)
 				return
 			}
@@ -76,7 +83,6 @@ func main() {
 				},
 			})
 			if digest, err := token.SignedString(priv_key); err != nil {
-				fmt.Println(err)
 				w.WriteHeader(http.StatusInternalServerError)
 			} else {
 				digest_bytes := []byte(digest)
@@ -86,9 +92,53 @@ func main() {
 			}
 		}
 	})
+	api_r.Get("/list/{dir_id}", func(w http.ResponseWriter, r *http.Request) {
+		token := strings.TrimPrefix(r.Header.Get("Authorization"), "Bearer ")
+		if auth, err := validate_jwt(token); err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+		} else {
+			server_url, _ := url.Parse(auth.Server)
+			c := client.SubsonicClient{
+				Server:   *server_url,
+				Username: auth.Username,
+				Password: auth.Password,
+			}
+			var entries []client.Entry
+			var err error
+			if dir_id := chi.URLParam(r, "dir_id"); dir_id[0] == 0x00 {
+				entries, err = c.Indexes()
+			} else {
+				entries, err = c.Dir(dir_id)
+			}
+
+			if err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+			} else {
+				data, _ := json.Marshal(entries)
+				w.Header().Add("Content-Type", "application/json")
+				w.Header().Add("Content-Length", fmt.Sprintf("%d", len(data)))
+				w.WriteHeader(http.StatusOK)
+				w.Write(data)
+			}
+		}
+	})
 	r.Mount("/api/", http.StripPrefix("/api/", api_r))
 
 	if err := http.ListenAndServe(listen_addr, r); err != nil {
 		fmt.Println(err)
+	}
+}
+
+func validate_jwt(token string) (*AuthInfo, error) {
+	decoded, err := jwt.ParseWithClaims(token, &CustomJWT{}, func(t *jwt.Token) (any, error) {
+		return pub_key, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	if info, ok := decoded.Claims.(*CustomJWT); !ok {
+		return nil, fmt.Errorf("Somehow signed an unnknown set of claims?")
+	} else {
+		return &info.Auth, nil
 	}
 }
