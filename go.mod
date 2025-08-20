0e2a625fba89ce63b22b1aceb2b9fda2dc3b9f93
Muaz Ahmad <mahmad2000@protonmail.com>
2025-08-20 18:54:34 +0500

    Remove all auth input from front-end

--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module subsonic-webui
 
 go 1.24.6
 
-require (
-	github.com/go-chi/chi/v5 v5.2.2
-	github.com/golang-jwt/jwt/v5 v5.3.0
-)
+require github.com/go-chi/chi/v5 v5.2.2
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"crypto/ed25519"
 	_ "embed"
 	"encoding/json"
 	"fmt"
@@ -9,35 +8,21 @@ import (
 	"net/http"
 	"net/url"
 	"os"
-	"strings"
 	"subsonic-webui/client"
 
 	"github.com/go-chi/chi/v5"
-	"github.com/golang-jwt/jwt/v5"
 )
 
 var (
 	//go:embed static/index.html
 	index []byte
-
-	priv_key ed25519.PrivateKey
-	pub_key  ed25519.PublicKey
 )
 
-func init() {
-	pub_key, priv_key, _ = ed25519.GenerateKey(nil)
-}
-
-type CustomJWT struct {
-	Auth AuthInfo
-	jwt.RegisteredClaims
-}
-
-type AuthInfo struct {
-	Server   string `json:"server"`
-	Username string `json:"username"`
-	Password string `json:"password"`
-}
+const (
+	SERVER   = "SUBSONIC_SERVER"
+	USERNAME = "SUBSONIC_USERNAME"
+	PASSWORD = "SUBSONIC_PASSWORD"
+)
 
 func main() {
 	var listen_addr string
@@ -45,6 +30,36 @@ func main() {
 		listen_addr = os.Args[1]
 	} else {
 		listen_addr = "localhost:8080"
+	}
+
+	server, ok := os.LookupEnv(SERVER)
+	if !ok {
+		fmt.Println("Missing server in ", SERVER)
+		os.Exit(1)
+	}
+	server_url, err := url.Parse(server)
+	if err != nil {
+		fmt.Println(server, " not a valid server URL")
+		os.Exit(1)
+	}
+	username, ok := os.LookupEnv(USERNAME)
+	if !ok {
+		fmt.Println("Missing Username in ", USERNAME)
+		os.Exit(1)
+	}
+	password, ok := os.LookupEnv(PASSWORD)
+	if !ok {
+		fmt.Println("Missing Username in ", PASSWORD)
+		os.Exit(1)
+	}
+	c := client.SubsonicClient{
+		Server:   *server_url,
+		Username: username,
+		Password: password,
+	}
+	if err := c.Ping(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
 
 	r := chi.NewRouter()
@@ -55,133 +70,48 @@ func main() {
 	})
 
 	api_r := chi.NewRouter()
-	api_r.Post("/login", func(w http.ResponseWriter, r *http.Request) {
-		d := json.NewDecoder(r.Body)
-		var auth AuthInfo
-		if err := d.Decode(&auth); err != nil {
+	api_r.Get("/list/{dir_id}", func(w http.ResponseWriter, r *http.Request) {
+		var entries []client.Entry
+		var err error
+		if dir_id := chi.URLParam(r, "dir_id"); dir_id[0] == 0x00 {
+			entries, err = c.Indexes()
+		} else {
+			entries, err = c.Dir(dir_id)
+		}
+
+		if err != nil {
 			w.WriteHeader(http.StatusBadRequest)
 		} else {
-			server_url, err := url.Parse(auth.Server)
-			if err != nil {
-				w.WriteHeader(http.StatusBadRequest)
-				return
-			}
-
-			c := client.SubsonicClient{
-				Server:   *server_url,
-				Username: auth.Username,
-				Password: auth.Password,
-			}
-			if err := c.Ping(); err != nil {
-				w.WriteHeader(http.StatusBadRequest)
-				return
-			}
-
-			token := jwt.NewWithClaims(&jwt.SigningMethodEd25519{}, CustomJWT{
-				auth,
-				jwt.RegisteredClaims{
-					Issuer: "subsonic-webui",
-				},
-			})
-			if digest, err := token.SignedString(priv_key); err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-			} else {
-				digest_bytes := []byte(digest)
-				w.Header().Add("Content-Type", "text/plain")
-				w.Header().Add("Content-Length", fmt.Sprintf("%d", len(digest_bytes)))
-				w.Write(digest_bytes)
-			}
-		}
-	})
-	api_r.Get("/list/{dir_id}", func(w http.ResponseWriter, r *http.Request) {
-		token := strings.TrimPrefix(r.Header.Get("Authorization"), "Bearer ")
-		if auth, err := validate_jwt(token); err != nil {
-			w.WriteHeader(http.StatusUnauthorized)
-		} else {
-			server_url, _ := url.Parse(auth.Server)
-			c := client.SubsonicClient{
-				Server:   *server_url,
-				Username: auth.Username,
-				Password: auth.Password,
-			}
-			var entries []client.Entry
-			var err error
-			if dir_id := chi.URLParam(r, "dir_id"); dir_id[0] == 0x00 {
-				entries, err = c.Indexes()
-			} else {
-				entries, err = c.Dir(dir_id)
-			}
-
-			if err != nil {
-				w.WriteHeader(http.StatusBadRequest)
-			} else {
-				data, _ := json.Marshal(entries)
-				w.Header().Add("Content-Type", "application/json")
-				w.Header().Add("Content-Length", fmt.Sprintf("%d", len(data)))
-				w.WriteHeader(http.StatusOK)
-				w.Write(data)
-			}
+			data, _ := json.Marshal(entries)
+			w.Header().Add("Content-Type", "application/json")
+			w.Header().Add("Content-Length", fmt.Sprintf("%d", len(data)))
+			w.WriteHeader(http.StatusOK)
+			w.Write(data)
 		}
 	})
 	api_r.Get("/stream/{song_id}", func(w http.ResponseWriter, r *http.Request) {
-		token := strings.TrimPrefix(r.Header.Get("Authorization"), "Bearer ")
-		if auth, err := validate_jwt(token); err != nil {
-			w.WriteHeader(http.StatusUnauthorized)
+		if resp, err := c.Stream(chi.URLParam(r, "song_id")); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
 		} else {
-			server_url, _ := url.Parse(auth.Server)
-			c := client.SubsonicClient{
-				Server:   *server_url,
-				Username: auth.Username,
-				Password: auth.Password,
-			}
-			if resp, err := c.Stream(chi.URLParam(r, "song_id")); err != nil {
-				w.WriteHeader(http.StatusBadRequest)
-			} else {
-				w.Header().Add("Content-Type", resp.Header.Get("Content-Type"))
-				w.Header().Add("Content-Length", resp.Header.Get("Content-Length"))
-				w.WriteHeader(http.StatusOK)
-				io.Copy(w, resp.Body)
-			}
+			w.Header().Add("Content-Type", resp.Header.Get("Content-Type"))
+			w.Header().Add("Content-Length", resp.Header.Get("Content-Length"))
+			w.WriteHeader(http.StatusOK)
+			io.Copy(w, resp.Body)
 		}
 	})
 	api_r.Get("/cover/{cover_id}", func(w http.ResponseWriter, r *http.Request) {
-		token := strings.TrimPrefix(r.Header.Get("Authorization"), "Bearer ")
-		if auth, err := validate_jwt(token); err != nil {
-			w.WriteHeader(http.StatusUnauthorized)
+		if resp, err := c.Cover(chi.URLParam(r, "cover_id")); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
 		} else {
-			server_url, _ := url.Parse(auth.Server)
-			c := client.SubsonicClient{
-				Server:   *server_url,
-				Username: auth.Username,
-				Password: auth.Password,
-			}
-			if resp, err := c.Cover(chi.URLParam(r, "cover_id")); err != nil {
-				w.WriteHeader(http.StatusBadRequest)
-			} else {
-				w.Header().Add("Content-Type", resp.Header.Get("Content-Type"))
-				w.Header().Add("Content-Length", resp.Header.Get("Content-Length"))
-				w.WriteHeader(http.StatusOK)
-				io.Copy(w, resp.Body)
-			}
+			w.Header().Add("Content-Type", resp.Header.Get("Content-Type"))
+			w.Header().Add("Content-Length", resp.Header.Get("Content-Length"))
+			w.WriteHeader(http.StatusOK)
+			io.Copy(w, resp.Body)
 		}
 	})
 	r.Mount("/api/", http.StripPrefix("/api/", api_r))
 
 	if err := http.ListenAndServe(listen_addr, r); err != nil {
 		fmt.Println(err)
-	}
-}
-
-func validate_jwt(token string) (*AuthInfo, error) {
-	decoded, err := jwt.ParseWithClaims(token, &CustomJWT{}, func(t *jwt.Token) (any, error) {
-		return pub_key, nil
-	})
-	if err != nil {
-		return nil, err
-	}
-	if info, ok := decoded.Claims.(*CustomJWT); !ok {
-		return nil, fmt.Errorf("Somehow signed an unnknown set of claims?")
-	} else {
-		return &info.Auth, nil
 	}
 }
