e4b506a7fb201d1fb3c1e789bee04173747fbba7
Muaz Ahmad <mahmad2000@protonmail.com>
2025-08-20 22:49:27 +0500

    some comments

--- client/client.go
+++ client/client.go
@@ -2,7 +2,6 @@ package client
 
 import (
 	"crypto/md5"
-
 	"encoding/json"
 	"fmt"
 	"math/rand"
@@ -29,6 +28,8 @@ var (
 	ErrNotMedia  = fmt.Errorf("Media endpoint returned non-media content")
 )
 
+// helper function, just checks if the response has the error field set, which means the response is invalid for
+// whatever is trying to call it. Also ensures any response is valid json, and fits the subsonic response schema
 func validate_api(r *http.Response) (*subsonicResponse, error) {
 	d := json.NewDecoder(r.Body)
 	var subsonic_r response
@@ -41,6 +42,7 @@ func validate_api(r *http.Response) (*subsonicResponse, error) {
 	return &subsonic_r.SubsonicResponse, nil
 }
 
+// Test connectivity and credentials
 func (c *SubsonicClient) Ping() error {
 	resp, err := c.get("/ping", nil)
 	if err != nil {
@@ -52,9 +54,12 @@ func (c *SubsonicClient) Ping() error {
 	return nil
 }
 
+// main func, appends needed params from https://opensubsonic.netlify.app/docs/api-reference/
+// performs the actual request and returns the response object, or an error
 func (c *SubsonicClient) get(path string, extra_params []param) (*http.Response, error) {
 	req_url := c.Server
 	req_url.Path, _ = url.JoinPath("rest", path)
+
 	q := make(url.Values)
 	q.Add("f", "json")
 	q.Add("v", "1.16.0")
@@ -80,6 +85,7 @@ func (c *SubsonicClient) get(path string, extra_params []param) (*http.Response,
 	return resp, nil
 }
 
+// fetches the index content, and transforms them into standard "Entry" objects
 func (c *SubsonicClient) Indexes() ([]Entry, error) {
 	resp, err := c.get("/getIndexes", nil)
 	if err != nil {
@@ -90,6 +96,7 @@ func (c *SubsonicClient) Indexes() ([]Entry, error) {
 	} else {
 		entries := make([]Entry, 0)
 		for _, i := range index_resp.Indexes.Index {
+			// All indexes are necessarily Dirs...
 			for _, j := range i.Artist {
 				entries = append(entries, Entry{
 					ID:      j.ID,
@@ -101,6 +108,7 @@ func (c *SubsonicClient) Indexes() ([]Entry, error) {
 			}
 		}
 		for _, i := range index_resp.Indexes.Child {
+			// ... but not all child's are files
 			entries = append(entries, Entry{
 				IsDir:   i.IsDir,
 				ID:      i.ID,
@@ -114,6 +122,8 @@ func (c *SubsonicClient) Indexes() ([]Entry, error) {
 	}
 }
 
+// /getMusicDirectory call
+// fetches dir content as a bunch of "Entry"s
 func (c *SubsonicClient) Dir(id string) ([]Entry, error) {
 	resp, err := c.get("/getMusicDirectory", []param{{
 		query: "id",
@@ -140,6 +150,7 @@ func (c *SubsonicClient) Dir(id string) ([]Entry, error) {
 	}
 }
 
+// "/stream"s a given song by its id.
 func (c *SubsonicClient) Stream(id string) (*http.Response, error) {
 	resp, err := c.get("/stream", []param{{
 		query: "id",
@@ -154,6 +165,7 @@ func (c *SubsonicClient) Stream(id string) (*http.Response, error) {
 	return resp, nil
 }
 
+// fetches cover art content by its url
 func (c *SubsonicClient) Cover(id string) (*http.Response, error) {
 	resp, err := c.get("/getCoverArt", []param{{
 		query: "id",
@@ -167,6 +179,9 @@ func (c *SubsonicClient) Cover(id string) (*http.Response, error) {
 	}
 	return resp, nil
 }
+
+// decoding and encoding structs.
+// see https://opensubsonic.netlify.app/docs/
 
 type response struct {
 	SubsonicResponse subsonicResponse `json:"subsonic-response"`
--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+
 	"subsonic-webui/client"
 
 	"github.com/go-chi/chi/v5"
@@ -25,6 +26,7 @@ const (
 )
 
 func main() {
+	// check input params
 	var listen_addr string
 	if len(os.Args) > 1 {
 		listen_addr = os.Args[1]
@@ -34,22 +36,22 @@ func main() {
 
 	server, ok := os.LookupEnv(SERVER)
 	if !ok {
-		fmt.Println("Missing server in ", SERVER)
+		fmt.Println("Missing server in", SERVER)
 		os.Exit(1)
 	}
 	server_url, err := url.Parse(server)
 	if err != nil {
-		fmt.Println(server, " not a valid server URL")
+		fmt.Println(server, "not a valid server URL")
 		os.Exit(1)
 	}
 	username, ok := os.LookupEnv(USERNAME)
 	if !ok {
-		fmt.Println("Missing Username in ", USERNAME)
+		fmt.Println("Missing Username in", USERNAME)
 		os.Exit(1)
 	}
 	password, ok := os.LookupEnv(PASSWORD)
 	if !ok {
-		fmt.Println("Missing Username in ", PASSWORD)
+		fmt.Println("Missing Username in", PASSWORD)
 		os.Exit(1)
 	}
 	c := client.SubsonicClient{
@@ -62,25 +64,31 @@ func main() {
 		os.Exit(1)
 	}
 
+	// this would ordinarily be its own class. This app is small enough to not warrant the extra effort
 	r := chi.NewRouter()
+	// serve the index.html file
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Content-Type", "text/html")
 		w.Header().Add("Content-Length", fmt.Sprintf("%d", len(index)))
 		w.Write(index)
 	})
 
+	// subrouter for all paths on /api/
 	api_r := chi.NewRouter()
 	api_r.Get("/list/{dir_id}", func(w http.ResponseWriter, r *http.Request) {
 		var entries []client.Entry
 		var err error
+		// if target dir is magic NULL byte (signifies root dir/indexes)
 		if dir_id := chi.URLParam(r, "dir_id"); dir_id[0] == 0x00 {
 			entries, err = c.Indexes()
 		} else {
 			entries, err = c.Dir(dir_id)
 		}
 
+		// Outside of network errors, this should never error in normal use
+		// Ideally there would be checking on the response here instead of a blanket response status
 		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
+			w.WriteHeader(http.StatusBadGateway)
 		} else {
 			data, _ := json.Marshal(entries)
 			w.Header().Add("Content-Type", "application/json")
@@ -91,7 +99,7 @@ func main() {
 	})
 	api_r.Get("/stream/{song_id}", func(w http.ResponseWriter, r *http.Request) {
 		if resp, err := c.Stream(chi.URLParam(r, "song_id")); err != nil {
-			w.WriteHeader(http.StatusBadRequest)
+			w.WriteHeader(http.StatusBadGateway)
 		} else {
 			w.Header().Add("Content-Type", resp.Header.Get("Content-Type"))
 			w.Header().Add("Content-Length", resp.Header.Get("Content-Length"))
@@ -101,7 +109,7 @@ func main() {
 	})
 	api_r.Get("/cover/{cover_id}", func(w http.ResponseWriter, r *http.Request) {
 		if resp, err := c.Cover(chi.URLParam(r, "cover_id")); err != nil {
-			w.WriteHeader(http.StatusBadRequest)
+			w.WriteHeader(http.StatusBadGateway)
 		} else {
 			w.Header().Add("Content-Type", resp.Header.Get("Content-Type"))
 			w.Header().Add("Content-Length", resp.Header.Get("Content-Length"))
